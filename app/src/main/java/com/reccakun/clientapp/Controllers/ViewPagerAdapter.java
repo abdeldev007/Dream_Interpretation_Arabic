db77ed3031620614462e5d4198ad22d44f79c72e
Abdallah id lahcen <abdallah.idlahcen2@gmail.com>
2019-09-29 20:02:08 +0100

    New Update

--- app/src/main/java/com/reccakun/clientapp/Controllers/ViewPagerAdapter.java
+++ app/src/main/java/com/reccakun/clientapp/Controllers/ViewPagerAdapter.java
@@ -2,13 +2,17 @@ package com.reccakun.clientapp.Controllers;
 
 
 import android.content.Context;
-import androidx.viewpager.widget.PagerAdapter;
-import android.view.LayoutInflater;
+ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.reccakun.clientapp.Models.Dream;
 import com.reccakun.clientapp.R;
@@ -24,13 +28,15 @@ public class ViewPagerAdapter extends PagerAdapter {
     List<Dream> ld=new ArrayList<>();
     Integer idDream;
     DBConnect dbDreams ;
-boolean  isFavActivity;
-    public ViewPagerAdapter(Context context,List<Dream> ld,Integer idDream,boolean isFavActivity) {
+ boolean  isFavActivity;
+ViewPager vp;
+    public ViewPagerAdapter(Context context, List<Dream> ld, Integer idDream, boolean isFavActivity, ViewPager vp ) {
         mContext = context;
         mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.idDream=idDream;
         this.ld=ld;
         this.isFavActivity=isFavActivity;
+this.vp=vp;
     }
 
     @Override
@@ -56,6 +62,26 @@ boolean  isFavActivity;
         dbDreams=new DBConnect(mContext.getApplicationContext());
         final ImageView btnAddFav = itemView.findViewById(R.id.btnAddFavorite);
         if (isFavActivity){
+            try{
+                ImageView btnNext=itemView.findViewById(R.id.btnNext);
+                ImageView btnPrev=itemView.findViewById(R.id.btnPrev);
+                btnNext.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        vp.setCurrentItem(vp.getCurrentItem()+1);
+                    }
+                });
+                btnPrev.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        vp.setCurrentItem(vp.getCurrentItem()-1);
+                    }
+                });
+            }catch (Exception e){
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
             if (dbDreams.isFavorite(ld.get(position).getDream_ID())){
                 btnAddFav.setImageResource(R.drawable.addheart );
               //  Toast.makeText(mContext, ""+dbDreams.isFavorite(ld.get(position).getDream_ID()), Toast.LENGTH_SHORT).show();
@@ -91,6 +117,27 @@ boolean  isFavActivity;
                 }
             });
         }else {
+            try{
+                ImageView btnNext=itemView.findViewById(R.id.btnNext);
+                ImageView btnPrev=itemView.findViewById(R.id.btnPrev);
+                btnNext.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        vp.setCurrentItem(vp.getCurrentItem()+1);
+                    }
+                });
+                btnPrev.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        vp.setCurrentItem(vp.getCurrentItem()-1);
+                    }
+                });
+            }catch (Exception e){
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
             if (dbDreams.isFavorite(ld.get(position).getDream_ID()))
                 btnAddFav.setImageResource(R.drawable.addheart );
             else
--- app/src/main/java/com/reccakun/clientapp/Views/ContentActivity.java
+++ app/src/main/java/com/reccakun/clientapp/Views/ContentActivity.java
@@ -1,33 +1,33 @@
 package com.reccakun.clientapp.Views;
 
-import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
-import android.os.Bundle;
+ import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.reccakun.clientapp.Controllers.DBConnect;
+import com.reccakun.clientapp.Models.CostumMenu;
+import com.reccakun.clientapp.Models.Dream;
+import com.reccakun.clientapp.Controllers.ViewPagerAdapter;
+import com.reccakun.clientapp.R;
+
+
+import android.annotation.TargetApi;
+        import android.os.Build;
+ import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.material.tabs.TabLayout;
-import com.reccakun.clientapp.Controllers.DBConnect;
-import com.reccakun.clientapp.Controllers.ViewPagerAdapter;
-import com.reccakun.clientapp.Models.CostumMenu;
-import com.reccakun.clientapp.Models.Dream;
-import com.reccakun.clientapp.R;
 
 import java.util.List;
 
@@ -35,124 +35,99 @@ public class ContentActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private Toolbar toolbar;
     private TabLayout tabLayout;
-    private Context mContext = ContentActivity.this;
+    private Context mContext=ContentActivity.this;
     List<Dream> ld;
     List<Dream> listDreams;
-    DBConnect dbDreams;
-    int idC, idD, idItem;
+    DBConnect dbDreams ;
+    int idC,idD,idItem;
     boolean isFavActivity;
-    ImageView imgBack;
-    private AdView mAdView;
-
+     ImageView imgBack;
     @TargetApi(Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
-        dbDreams = new DBConnect(getApplicationContext());
+        dbDreams=new DBConnect(getApplicationContext());
+try{
+      this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+    getSupportActionBar().setDisplayShowCustomEnabled(true);
+    getSupportActionBar().setCustomView(R.layout.custom_action_bar);
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
+    getSupportActionBar().setElevation(5);
+    View view = getSupportActionBar().getCustomView();
+      imgBack=view.findViewById(R.id.backitem);
 
-        mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+    this.getSupportActionBar().setBackgroundDrawable(new ColorDrawable( Color.parseColor("#3F51B5")));
+    ImageView imgFav=view.findViewById(R.id.favitem);
+    imgFav.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-        try {
-            this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-            getSupportActionBar().setDisplayShowCustomEnabled(true);
-            getSupportActionBar().setCustomView(R.layout.custom_action_bar);
-
-            getSupportActionBar().setElevation(5);
-            View view = getSupportActionBar().getCustomView();
-            imgBack = view.findViewById(R.id.backitem);
-
-            this.getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3F51B5")));
-            ImageView imgFav = view.findViewById(R.id.favitem);
-            imgFav.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent intent = new Intent(ContentActivity.this, FavoriteActivity.class);
-                    startActivity(intent);
-                }
-            });
-            ImageView home = findViewById(R.id.homeBtn);
-            home.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent i = new Intent(ContentActivity.this, MainActivity.class);
-                    startActivity(i);
-                }
-            });
-            dbDreams = new DBConnect(getApplicationContext());
-            Intent intent = getIntent();
-            idC = (int) intent.getSerializableExtra("idCat");
-            idD = (int) intent.getSerializableExtra("idDream");
-            idItem = (int) intent.getSerializableExtra("idItem");
-            isFavActivity = (boolean) intent.getSerializableExtra("isFav");
-            if (isFavActivity) {
-                listDreams = dbDreams.getFavDreams();
-                ViewPagerAdapter adapter = new ViewPagerAdapter(mContext, listDreams, idD, true);
-                viewPager = (ViewPager) findViewById(R.id.pager);
-                viewPager.setAdapter(adapter);
-                viewPager.setCurrentItem(idItem);
-                imgBack.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        onBackPressed();
-
-                    }
-                });
-
-            } else {
-
-                listDreams = dbDreams.getDreamsByCat(idC);
-                ViewPagerAdapter adapter = new ViewPagerAdapter(mContext, listDreams, idD, false);
-                viewPager = (ViewPager) findViewById(R.id.pager);
-                viewPager.setAdapter(adapter);
-                viewPager.setCurrentItem(idItem);
-                imgBack.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        onBackPressed();
-
-                    }
-                });
-
-            }
-            ImageView popUp = findViewById(R.id.triple_dots);
-            CostumMenu costumMenu = new CostumMenu(mContext, popUp);
-            costumMenu.PopUpMenu();
-            ImageView backward = findViewById(R.id.backward);
-            backward.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
-
-                }
-            });
-            ImageView forward = findViewById(R.id.forward);
-            forward.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
-
-                }
-            });
-
-            SearchView searchView = findViewById(R.id.searchBar);
-            searchView.setVisibility(View.GONE);
-        } catch (Exception e) {
-            Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(ContentActivity.this,FavoriteActivity.class);
+            startActivity(intent);
         }
+    });
+    ImageView home=findViewById(R.id.homeBtn);
+    home.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent i=new Intent(ContentActivity.this,MainActivity.class);
+            startActivity(i);
+        }
+    });
+        dbDreams=new DBConnect(getApplicationContext());
+          Intent intent = getIntent();
+          idC = (int)  intent.getSerializableExtra("idCat");
+          idD  = (int)   intent.getSerializableExtra("idDream");
+          idItem  = (int)   intent.getSerializableExtra("idItem");
+    isFavActivity  = (boolean)   intent.getSerializableExtra("isFav");
+        if (isFavActivity){
+            listDreams=dbDreams.getFavDreams();
+            viewPager = (ViewPager) findViewById(R.id.pager);
+
+            ViewPagerAdapter adapter = new ViewPagerAdapter(mContext,listDreams,idD,true,viewPager);
+            viewPager.setAdapter(adapter);
+            viewPager.setCurrentItem(idItem );
+            imgBack.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    onBackPressed();
+
+                }
+            });
+
+        }else {
+            viewPager = (ViewPager) findViewById(R.id.pager);
+
+          listDreams=dbDreams.getDreamsByCat(idC);
+            ViewPagerAdapter adapter = new ViewPagerAdapter(mContext,listDreams,idD,false,viewPager);
+            viewPager.setAdapter(adapter);
+            viewPager.setCurrentItem(idItem );
+            imgBack.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    onBackPressed();
+
+                }
+            });
+
+        }
+    ImageView popUp=findViewById(R.id.triple_dots);
+    CostumMenu costumMenu=new CostumMenu(mContext,popUp);
+    costumMenu.PopUpMenu();
+
+
+        SearchView searchView=findViewById(R.id.searchBar);
+        searchView.setVisibility(View.GONE);
+     }catch (Exception e){
+         Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
     }
+    }
+
+
 
 
 }
